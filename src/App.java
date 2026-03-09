b6706e04ba96fc2fd690e02bfac95b2c8ca548a0
Mondragon <mondragonjacerrein@gmail.com>
2026-03-09 20:12:32 +0800

    Testing

--- src/App.java
+++ src/App.java
@@ -19,10 +19,12 @@ public class App{
         System.out.println("\nGame Ready!");
 
         System.out.println("\n-----------System------------");
-        System.out.print("Choose your Character:\n[0]Seth\n[1]Seraph\n[2]Aurelius\n[3]Agnis\n[4]Verlierer\n[5]Fvyina\nChoose: ");
+        System.out.print("Choose your Character: [1]Nash\n[2]Vincent\n[3]Cid");
         int chooseCharacter = scanner.nextInt();
         scanner.nextLine();
 
+        System.out.println("Testing");
 
+        
     }
 }
